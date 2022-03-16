220ef4a2821b936b9df4cfaee795204298788d8d
RomeoWeb <stelsnsns@gmail.com>
2022-03-16 20:27:21 +0200

    lesson 21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,22 @@ import d from './Dialogs.module.css';
 
 const Dialogs = (props)=> {
    return (
-      <div>
-         <h2 className={d.wrapper}>Dialogs</h2>
+      <div className={d.wrapper}>
+         <h2 className={d.title}>Dialogs</h2>
+         <div className={d.wrapperInner}>
+            <div className={d.dialogs}>
+               <div className={d.dialogsItem + ' '+ d.active}>Dmytriy</div>
+               <div className={d.dialogsItem}>Andrey</div>
+               <div className={d.dialogsItem}>Sveta</div>
+               <div className={d.dialogsItem}>Olya</div>
+            </div>
+            <div className={d.messages}>
+               <div className={d.messagesItem}>Hi</div>
+               <div className={d.messagesItem}>How are you</div>
+               <div className={d.messagesItem}>What did you do yesterday?</div>
+               <div className={d.messagesItem}>Have a nice day</div>
+            </div>
+         </div>
       </div>
    )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
           <li className={s.item}><NavLink to ='/profile' className={navData=>navData.isActive ? s.active : s.item}>Profile</NavLink></li>
           <li className={`${s.item} ${s.active}`}><NavLink to='/dialogs' className={navData=>navData.isActive ? s.active : s.item}>Messages</NavLink></li>
           <li className={s.item}>News</li>
-          <li className={s.item}>Massages 2</li>
+          <li className={s.item}>Contacts</li>
         </ul>
     </nav>
    )
