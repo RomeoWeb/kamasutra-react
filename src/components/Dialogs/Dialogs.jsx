017d314f5c9de03812d3b877bf0376139fadecaa
RomeoWeb <stelsnsns@gmail.com>
2022-03-25 16:33:47 +0200

    lesson 25

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -32,25 +32,20 @@ const Dialogs = (props)=> {
       {id: 4, message: 'Have a nice day'}
       ]
 
+   let dialog = dialogsData.map(d => <DialogItem name = {d.name} id = {d.id}/> )
+   let message = messagesData.map(m =>  <Message message = {m.message}/>)
+
    return (
       <div className={d.wrapper}>
          <h2 className={d.title}>Dialogs</h2>
          <div className={d.wrapperInner}>
             <div className={d.dialogs}>
 
-               <DialogItem name = {dialogsData[0].name} id = {dialogsData[0].id}/>
-               <DialogItem name = {dialogsData[1].name} id = {dialogsData[1].id}/>
-               <DialogItem name = {dialogsData[2].name} id = {dialogsData[2].id}/>
-               <DialogItem name = {dialogsData[3].name} id = {dialogsData[3].id}/>
-               <DialogItem name = {dialogsData[4].name} id = {dialogsData[4].id}/>
-               <DialogItem name = {dialogsData[5].name} id = {dialogsData[5].id}/>
+               {dialog}
                
             </div>
             <div className={d.messages}>
-               <Message message = {messagesData[0].message}/>
-               <Message message = {messagesData[1].message}/>
-               <Message message = {messagesData[2].message}/>
-               <Message message = {messagesData[3].message}/>
+               {message}
             </div>
          </div>
       </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -10,6 +10,7 @@ const MyPosts = (props) => {
     {id: 3, post: 'What did you do yesterday?', likesCount: 50},
     {id: 4, post: 'Have a nice day', likesCount: 30}
     ]
+    let post = postsData.map(p =>  < Post message = { p.post} likesCount = { p.likesCount }/>)
    return (
     <div className={s.posts_wrapper}>
       <h3>My Posts</h3>
@@ -20,10 +21,7 @@ const MyPosts = (props) => {
         <button>Add Post</button>
       </div>
       <div className = {s.my_posts}>
-        < Post message = {postsData[0].post} likesCount = {postsData[0].likesCount}/>
-        < Post message = {postsData[1].post} likesCount = {postsData[1].likesCount}/>
-        < Post message = {postsData[2].post} likesCount = {postsData[2].likesCount}/>
-        < Post message = {postsData[3].post} likesCount = {postsData[3].likesCount}/>
+        { post }
       </div>
     </div>
    )
