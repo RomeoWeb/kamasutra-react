f2c9b79d77e56201b1d750c6abbcd812d30e08ed
RomWeb <stelsnsns@gmail.com>
2022-04-07 12:32:12 +0300

    lesson 31

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,9 +1,17 @@
+import { useRef } from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 
 const MyPosts = (props) => {
-  // debugger;
+
+  let newPostElement = useRef();
+  //let newPostElement = React.creatRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  }
 
    let post =
    props.post.map(p =>  < Post message = { p.post} likesCount = { p.likesCount }/>)
@@ -11,10 +19,10 @@ const MyPosts = (props) => {
     <div className={s.posts_wrapper}>
       <h3>My Posts</h3>
       <div>
-        <textarea></textarea>
+        <textarea ref={newPostElement}></textarea>
       </div>
       <div>
-        <button>Add Post</button>
+        <button onClick={addPost}>Add Post</button>
       </div>
       <div className = {s.my_posts}>
         { post }
