bf976f52056b14d326bd9222b2f055e3e7a8d533
RomWeb <stelsnsns@gmail.com>
2022-04-09 09:26:46 +0300

    lesson 33

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import {rerenderEntireTree} from '../render';
+
 let state = {
    profilePage: {
       posts : [
@@ -33,5 +35,7 @@ export let addPost = (postMessage) => {
       likesCount: 30
    }
    state.profilePage.posts.push(newPost);
+   rerenderEntireTree(state);
+   
 }
 export default state;
